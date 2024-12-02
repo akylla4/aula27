26372b04f5be2fcc05620345c75b139e653aec1e
vitoria <vitoria.a.lima10@aluno.senai.br>
2024-12-02 11:19:49 -0500

    alteracao do dia

--- revisao.py
+++ revisao.py
@@ -8,8 +8,28 @@ nota4 = float (input())
 faltas = int (input())
 #calculo da media 
 media = (nota1+nota2+nota3+nota4)/4
-print (media)
 
+#logica da situacao 
+if faltas > 31:
+    situacao = "reprovado por faltas"
+elif media >= 8:
+    situacao = "aprovado"
+elif media >= 5: "recuperacao"
+recuperacao = float (input())#LER A NOTA DA PROVA DE RECUPERACAO
+if recuperacao >=(10-media):
+        situacao = "aprovado na recuperacao"
+else:
+     situacao = "reprovado na recuperacao"
+else:
+situacao = "reprovado por media"
+
+#relatorio
+print ("nome:",nome)
+print ("notas",nota1,nota2,nota3,nota4)
+print("faltas",faltas)
+print("media",media)
+print ('situacao',situacao) 
+ 
 
 
 

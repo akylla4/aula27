c22a722b4dc4c16f60b57e0a2c5effcb3983b634
vitoria <vitoria.a.lima10@aluno.senai.br>
2024-12-02 09:37:04 -0500

    um novo comando

---
+++ revisao.py
@@ -0,0 +1,66 @@
+#LER ENTRADAS DO USUÁRIO 
+nome = input() #ARMAZENAR o nome do aluno 
+nota1 = float (input()) # 4 NOTAS DO ALUNO
+nota2 = float (input())
+nota3 = float (input())
+nota4 = float (input())
+
+faltas = int (input())
+#calculo da media 
+media = (nota1+nota2+nota3+nota4)/4
+print (media)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

503be7b4508f3f1d420a5c2e3ac0612720826c4b
vitoria <vitoria.a.lima10@aluno.senai.br>
2024-12-04 10:05:20 -0500

    alteracao perfeita

--- revisao.py
+++ revisao.py
@@ -1,6 +1,8 @@
 #LER ENTRADAS DO USUÁRIO 
+situacao = ""
+aluno = [] #lista que guardara todos os alunos  cadastrar 
 cont = 0
-escolha_usuario = int (input()) # variavel que guarda quantas vezes o codigo vai rodar
+escolha_usuario = int (input("Digite quantos alunos voce quer cadastrar")) # variavel que guarda quantas vezes o codigo vai rodar
 while cont < escolha_usuario:
     nome = input() #ARMAZENAR o nome do aluno 
     nota1 = float (input()) # 4 NOTAS DO ALUNO
@@ -13,28 +15,24 @@ while cont < escolha_usuario:
     media = (nota1+nota2+nota3+nota4)/4
 
     #logica da situacao 
-if faltas > 31:
-     situacao = "reprovado por faltas"
-elif media >= 8:
-    situacao = "aprovado"
-elif media >= 5:
-    situacao = "recuperacao"
-    recuperacao = float (input()) #LER A NOTA DA PROVA DE RECUPERACAO 
-    if recuperacao >=(10-media):
-      situacao = "aprovado na recuperacao"
+    if faltas > 31:
+        situacao = "reprovado por faltas"
+    elif media >= 8:
+        situacao = "aprovado"
+    elif media >= 5:
+        situacao = "recuperacao"
+        recuperacao = float (input()) #LER A NOTA DA PROVA DE RECUPERACAO 
+        if recuperacao >=(10-media):
+            situacao = "aprovado na recuperacao"
+        else:
+            situacao = "reprovado na recuperacao"
     else:
-        situacao = "reprovado na recuperacao"
-else:
-    situacao = "reprovado por media"
+        situacao = "reprovado por media"
+        #enviar os dados do aluno para a lista alunos 
+    aluno.append([nome,faltas,media,situacao]) 
+    cont += 1 
     #relatorio
-    print ("nome:",nome)
-    print ("notas",nota1,nota2,nota3,nota4)
-    print("faltas",faltas)
-    print("media",media)
-    print ('situacao',situacao) 
-    cont = cont + 1
-    
-
+print(aluno)
 
 
 

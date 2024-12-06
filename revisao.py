d99187766b3792cb5cd29c2d1edc5a725e568a89
vitoria <vitoria.a.lima10@aluno.senai.br>
2024-12-06 09:29:22 -0500

    concluido

--- revisao.py
+++ revisao.py
@@ -1,20 +1,20 @@
 #LER ENTRADAS DO USUÁRIO 
 aluno = []
-while True :
+while True : # INFINITO 
      #lista que guardara todos os alunos  cadastrar 
     #ler a entrada do usuário 
-    escolha_menu =int (input()) #variavel que guarda qual opção do menu o úsario escolheu   
+    escolha_menu =int (input("escolha uma opção 1 cadastro: - 2 relatório: -3 para encerrar:")) #variavel que guarda qual opção do menu o úsario escolheu   
     if escolha_menu == 1: #se a escolha 
         situacao = ""
     
         cont = 0 # variavel que controla a repetição 
-        escolha_usuario = int (input("Digite quantos alunos voce quer cadastrar")) # variavel que guarda quantas vezes o codigo vai rodar
+        escolha_usuario = int (input("Digite quantos alunos voce quer cadastrar:")) # variavel que guarda quantas vezes o codigo vai rodar
         while cont < escolha_usuario:
-            nome = input() #ARMAZENAR o nome do aluno 
-            nota1 = float (input("nota")) # 4 NOTAS DO ALUNO
-            nota2 = float (input("nota"))
-            nota3 = float (input("nota"))
-            nota4 = float (input("nota"))
+            nome = input("digite o nome do aluno:") #ARMAZENAR o nome do aluno 
+            nota1 = float (input(" digite nota aluno:")) # 4 NOTAS DO ALUNO
+            nota2 = float (input("digite nota aluno:"))
+            nota3 = float (input("digite nota aluno:"))
+            nota4 = float (input("digite nota aluno:"))
 
             faltas = int (input("faltas"))
             #calculo da media 
@@ -37,7 +37,8 @@ while True :
             cont += 1 
     elif escolha_menu == 2:  #relatorio
         print(aluno)
-
+    elif escolha_menu == 3: #se o usuario escolheu 
+        break # quebra a execução do enquanto 
 
 
 

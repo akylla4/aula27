3d8601d79b4c0111acaaf2ca0d4a0bebf53723ed
vitoria <vitoria.a.lima10@aluno.senai.br>
2024-12-11 09:59:59 -0500

    funcaofeita

--- funcao.py
+++ funcao.py
@@ -1,13 +1,15 @@
 #DECLARAR UMA FUNÇÃO 
-def bom_dia (): 
-    print ("BOM DIA!")
+def saudacoes (hora_do_dia): #exibir a saudação corresponde a hora 
+#DAR BOM DIA
+    if hora_do_dia >= 0 and (hora_do_dia <= 12) : 
+        print ("bom dia!!!")
+    elif (hora_do_dia >= 13) and (hora_do_dia <= 18) : 
+        print ("boa tarde!")
+    else:
+        print("boa noite")
+# FORA DA FUNÇÃO 
+# peço para o usuário dizer a hora atual 
+resposta = int (input("digite que horas sao:\n"))
+#CHAMO A FUNÇÃO PASSANDO PARA ELA O PARAMETRO OBRIGATIVO 
+saudacoes (resposta)
 
-
-def boa_noite ():
-    print("BOA NOITE!")
-bom_dia() 
-boa_noite()
-
-def boa_tarde ():
-    print("boa tarde!")
-boa_tarde()

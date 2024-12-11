41557c4598804107b3f46e63a808603f68b6e44b
vitoria <vitoria.a.lima10@aluno.senai.br>
2024-12-11 08:49:27 -0500

    funcaofeita

---
+++ funcao.py
@@ -0,0 +1,13 @@
+#DECLARAR UMA FUNÇÃO 
+def bom_dia (): 
+    print ("BOM DIA!")
+
+
+def boa_noite ():
+    print("BOA NOITE!")
+bom_dia() 
+boa_noite()
+
+def boa_tarde ():
+    print("boa tarde!")
+boa_tarde()
